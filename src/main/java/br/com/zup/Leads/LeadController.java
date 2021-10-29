b536ffc823a7b6ee258b8a9360644adf8798de12
Anna Karolyne <dev.akvc@gmail.com>
2021-10-29 14:03:49 -0300

    Criar classe LeadController

--- src/main/java/br/com/zup/Leads/LeadController.java
+++ src/main/java/br/com/zup/Leads/LeadController.java
@@ -1,7 +1,29 @@
 package br.com.zup.Leads;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.Leads.dto.LeadDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/leads")
 public class LeadController {
+
+    @Autowired
+    private LeadService leadService;
+
+    @GetMapping
+    public List<LeadDTO> exibirLeads() {
+        return leadService.exibirLeads();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarLead(@RequestBody LeadDTO leadDTO){
+        leadService.salvarLeads(leadDTO);
+    }
+
+
 }
