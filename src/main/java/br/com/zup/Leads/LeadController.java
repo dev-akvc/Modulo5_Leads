1c2a0c11dc0637777e9cc9e8573529ba4b5977d3
Anna Karolyne <dev.akvc@gmail.com>
2021-10-29 12:04:40 -0300

    Colocar anotações @Service e @RestController

--- src/main/java/br/com/zup/Leads/LeadController.java
+++ src/main/java/br/com/zup/Leads/LeadController.java
@@ -1,4 +1,7 @@
 package br.com.zup.Leads;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LeadController {
 }
--- src/main/java/br/com/zup/Leads/LeadService.java
+++ src/main/java/br/com/zup/Leads/LeadService.java
@@ -1,4 +1,7 @@
 package br.com.zup.Leads;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LeadService {
 }
