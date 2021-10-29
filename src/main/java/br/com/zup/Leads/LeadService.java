41c705c27d91112e76e9cb4ff3a2d7e24323d394
Anna Karolyne <dev.akvc@gmail.com>
2021-10-29 13:50:19 -0300

    Criar classe LeadService

--- src/main/java/br/com/zup/Leads/LeadService.java
+++ src/main/java/br/com/zup/Leads/LeadService.java
@@ -1,7 +1,33 @@
 package br.com.zup.Leads;
 
+import br.com.zup.Leads.dto.LeadDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LeadService {
+
+    private List<LeadDTO> listaLeads = new ArrayList<>();
+
+    public List<LeadDTO> exibirLeads() {
+        return listaLeads;
+    }
+
+    public void salvarLeads(LeadDTO leadDTO) {
+        listaLeads.add(leadDTO);
+    }
+
+    public LeadDTO pesquisarLead(String nomeLead) {
+        for(LeadDTO objeto : listaLeads){
+            if(objeto.getNome().equals(nomeLead)){
+                return objeto;
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado");
+    }
+
 }
