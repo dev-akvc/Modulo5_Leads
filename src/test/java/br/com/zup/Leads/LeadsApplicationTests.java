368ef995a2b58c548e61b1bf3afd6a5e21439b0a
Anna Karolyne <dev.akvc@gmail.com>
2021-10-29 11:29:19 -0300

    Primeiro commit

---
+++ src/test/java/br/com/zup/Leads/LeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Leads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
